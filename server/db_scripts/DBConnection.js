41bd0f6f42261a7e5bcc980be95987513315e459
Android009 <andr3y.2010@yahoo.com>
2017-08-10 18:30:03 +0300

    Made Card display + rounded card corners

--- server/db_scripts/DBConnection.js
+++ server/db_scripts/DBConnection.js
@@ -1,23 +1,23 @@
 class DBConnection {
-	constructor() {
-		this.sql = require("mssql");
-		this.config = {
-			user: "a",
-			password: "a",
-			server: "localhost",
-			database: "CardGame",
-			port: 1848
-		};
-	}
+    constructor() {
+        this.sql = require("mssql");
+        this.config = {
+            user: "test",
+            password: "test",
+            server: "localhost",
+            database: "CardGame",
+            port: 50085
+        };
+    }
 
-	initialize() {
-		this.sql.connect(this.config, err => {
-			if (err) {
-				res.status(500).send(err);
-				this.sql.close();
-			}
-		});
-	}
+    initialize() {
+        this.sql.connect(this.config, err => {
+            if (err) {
+                res.status(500).send(err);
+                this.sql.close();
+            }
+        });
+    }
 }
 
 module.exports = DBConnection;
