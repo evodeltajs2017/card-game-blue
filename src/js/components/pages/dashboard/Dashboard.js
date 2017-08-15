45dead5e91ad6003bcc915db595f83cfe65642a1
Android009 <andr3y.2010@yahoo.com>
2017-08-15 19:08:51 +0300

    Created board layout

--- src/js/components/pages/dashboard/Dashboard.js
+++ src/js/components/pages/dashboard/Dashboard.js
@@ -5,8 +5,53 @@ class Dashboard {
 
     initialize() {
         const div = document.createElement("div");
-        div.innerHTML = "<h1 class='dashboard'>Dashboard</h1>";
+        this.domElement = div;
+        div.className = "StartDiv";
+        
+        const startButton = document.createElement("button");
+        startButton.className = "startButton";
+        startButton.addEventListener("click",() =>{
+            this.startGame();
+        });
+
+        
+        startButton.textContent = "Start New Game";
+
+        this.container.appendChild(startButton);
         this.container.appendChild(div);
+    }
+
+    startGame(){
+        
+        const gameDiv = document.querySelector(".StartDiv");
+        gameDiv.className = "GameDiv";
+        document.querySelector(".startButton").remove();
+
+        let template=`
+        <div class="OpponentHand">
+            <div class="OpponentMana">
+                <div class="Mana"></div>
+            </div>
+        </div>
+        <div class ="Info">
+            
+            <div class="OpponentDeck"></div>
+            <div class="EndTurnButton"></div>
+            <div class="PlayerDeck"></div>
+            
+        </div>
+        <div class="OpponentField"></div>
+    
+        <div class="PlayerField"></div>
+        <div class="PlayerHand">
+            <div class="PlayerMana">
+                <div class="Mana"></div>
+            </div>
+         </div>
+    
+        `;
+
+        gameDiv.innerHTML = template;
     }
 
     destroy() {}
