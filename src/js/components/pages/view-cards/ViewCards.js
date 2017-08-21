78fcfbbc853a05c31f00b8dceb54c05210ec888b
Alexandra Docolin <aly_docolin@yahoo.com>
2017-08-21 15:10:45 +0300

    Update ViewCards.js

--- src/js/components/pages/view-cards/ViewCards.js
+++ src/js/components/pages/view-cards/ViewCards.js
@@ -2,26 +2,61 @@ class ViewCards {
     constructor(container) {
         this.container = container;
         this.number = 1;
+        this.func = null;
+        this.searchname = "";
+        this.totalcards = 0;
     }
 
-    putCardsInPlace(openDiv) {
+    putCardsInPlace() {
         const repo = new ViewCardsRepository();
+        const cardsDiv = document.querySelector(".cards");
         const context = this;
-        repo.getPageOfCards(context.number, (status, data) => {
+        repo.getPageOfCards(context.number, context.searchname, (status, data) => {
             if (status !== 200) {
 
             } else {
                 console.log(data);
+                context.totalcards = data.length;
+                context.number += data.length;
                 for (var i = data.length - 1; i >= 0; i--) {
                     let div = document.createElement("div");
+                    div.className = "parentOfCard";
                     div.style.height = "400px";
                     div.style.width = "300px";
-                    let card = new Card(div, 250, 350, 0, 0, 0, 0);
+                    let card = new Card(div, 250, 350, data[i].Name, data[i].Cost, data[i].Damage, data[i].Health);
                     card.initialize();
-                    openDiv.appendChild(div);
+                    cardsDiv.appendChild(div);
                 }
             }
         });
+    }
+
+    search() {
+        const input = document.getElementsByTagName("input")[1].value;
+        this.searchname = input;
+        this.number = 1;
+        const parent = document.getElementsByClassName("content-container")[0];
+        let cards = document.getElementsByClassName("parentOfCard");
+        let cardsDiv = document.getElementsByClassName("cards")[0];
+        cardsDiv.innerHTML = '';
+        this.putCardsInPlace();
+    }
+
+    addScrollEvent() {
+        const onscroll = (e) => {
+            const top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+            const limit = document.documentElement.clientHeight + 300;
+            const context = this;
+            if (document.documentElement.scrollHeight - top < limit)
+            {
+                if (context.totalcards < 24) { }
+                else { 
+                    this.putCardsInPlace(); 
+                }
+            }
+        };
+        this.handlescroll = onscroll;
+        window.addEventListener("scroll", this.handlescroll, false);
     }
 
     buildThePage() {
@@ -38,25 +73,34 @@ class ViewCards {
         field.className = "toTheRight";
         field.setAttribute("placeholder", "Card name...");
         div.appendChild(field);
+        button.addEventListener("click", (e) => { this.search(); }, false);
 
         div.style.textAlign = "center";
         this.container.appendChild(div);
+
+        const cardsDiv = document.createElement("div");
+        cardsDiv.className = "cards";
+
+        const backImgInner = document.createElement("div");
+        backImgInner.className = "backImgInner";
+        cardsDiv.appendChild(backImgInner);
 
         const backImg = document.createElement("div");
         backImg.className = "backImg";
         this.container.appendChild(backImg);
 
-        const openDiv = document.createElement("div");
-        openDiv.className = "cards";
-
-        this.container.appendChild(openDiv);
-
-        this.putCardsInPlace(openDiv);
+        this.container.appendChild(cardsDiv);
     }
 
     initialize() {
         this.buildThePage();
+
+        this.putCardsInPlace();
+
+        this.addScrollEvent();
     }
 
-    destroy() {}
+    destroy() {
+        window.removeEventListener("scroll", this.handlescroll);
+    }
 }
